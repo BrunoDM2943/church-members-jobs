03691df8b3a0b85486a3272a8687e24a7fb012b6
BrunoDM2943 <bdm2943@gmail.com>
2021-07-19 16:46:00 -0300

    Fixing test

--- test/test_notification.py
+++ test/test_notification.py
@@ -1,15 +1,31 @@
+import os
 from unittest.mock import patch
 
 import boto3
+import pytest
 from moto import mock_sns
 
 from app.notification import send_notification
 
 
-@mock_sns
-def test_send_notification():
-    sns = boto3.resource('sns', region_name="us-east-1")
-    topic = sns.create_topic(Name='test')
+@pytest.fixture(scope='function')
+def aws_credentials():
+    """Mocked AWS Credentials for moto."""
+    os.environ['AWS_ACCESS_KEY_ID'] = 'testing'
+    os.environ['AWS_SECRET_ACCESS_KEY'] = 'testing'
+    os.environ['AWS_SECURITY_TOKEN'] = 'testing'
+    os.environ['AWS_SESSION_TOKEN'] = 'testing'
+
+
+@pytest.fixture(scope='function')
+def topic():
+    with mock_sns():
+        sns = boto3.resource('sns', region_name="us-east-1")
+        topic = sns.create_topic(Name='test')
+        yield topic
+
+
+def test_send_notification(topic):
     with patch('app.notification.getenv') as patched_getenv:
         patched_getenv.return_value = topic.arn
         send_notification("test")
--- test/test_repository.py
+++ test/test_repository.py
@@ -1,45 +1,56 @@
 import datetime
+import os
 
 import boto3
+import pytest
 from moto import mock_dynamodb2
 
 from app.repository import convert_date, find_last_birthdays, find_last_marriages
 
 
-def build_mock_dynamodb():
-    dynamodb = boto3.resource("dynamodb",  region_name="us-east-1")
-    dynamodb.create_table(
-        TableName='member',
-        KeySchema=[
-            {
-                "AttributeName": "id",
-                "KeyType": "HASH"
-            }
-        ],
-        AttributeDefinitions=[
-            {
-                'AttributeName': 'id',
-                'AttributeType': 'N'
-            },
-        ]
-    )
-    return dynamodb
+@pytest.fixture(scope='function')
+def aws_credentials():
+    """Mocked AWS Credentials for moto."""
+    os.environ['AWS_ACCESS_KEY_ID'] = 'testing'
+    os.environ['AWS_SECRET_ACCESS_KEY'] = 'testing'
+    os.environ['AWS_SECURITY_TOKEN'] = 'testing'
+    os.environ['AWS_SESSION_TOKEN'] = 'testing'
+
+
+@pytest.fixture(scope='function')
+def dynamodb():
+    with mock_dynamodb2():
+        dynamodb = boto3.resource("dynamodb", region_name="us-east-1")
+        dynamodb.create_table(
+            TableName='member',
+            KeySchema=[
+                {
+                    "AttributeName": "id",
+                    "KeyType": "HASH"
+                }
+            ],
+            AttributeDefinitions=[
+                {
+                    'AttributeName': 'id',
+                    'AttributeType': 'N'
+                },
+            ]
+        )
+        yield dynamodb
 
 
 def test_convert_date():
     formatted = convert_date(datetime.datetime.strptime("2020-05-10", "%Y-%m-%d"))
     assert "05-10" == formatted
 
-@mock_dynamodb2
-def test_find_last_birthdays_empty():
-    build_mock_dynamodb()
+
+def test_find_last_birthdays_empty(dynamodb):
     now = datetime.date.today()
     members = find_last_birthdays(now - datetime.timedelta(days=7), now)
     assert 0 == len(members)
 
-@mock_dynamodb2
-def test_find_last_birthdays_with_values():
-    dynamodb = build_mock_dynamodb()
+
+def test_find_last_birthdays_with_values(dynamodb):
     table = dynamodb.Table('member')
     table.put_item(Item={
         "id": 'xx',
@@ -52,16 +63,14 @@ def test_find_last_birthdays_with_values():
     members = find_last_birthdays(now - datetime.timedelta(days=7), now)
     assert 1 == len(members)
 
-@mock_dynamodb2
-def test_find_last_marriages_empty():
-    build_mock_dynamodb()
+
+def test_find_last_marriages_empty(dynamodb):
     now = datetime.date.today()
     members = find_last_marriages(now - datetime.timedelta(days=7), now)
     assert 0 == len(members)
 
-@mock_dynamodb2
-def test_find_last_birthdays_with_values():
-    dynamodb = build_mock_dynamodb()
+
+def test_find_last_birthdays_with_values(dynamodb):
     table = dynamodb.Table('member')
     table.put_item(Item={
         "id": 'xx',
