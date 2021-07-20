2071c3dc6a72f7ca4bc74d97af85d28cce1ef9e9
BrunoDM2943 <bdm2943@gmail.com>
2021-07-19 23:09:29 -0300

    Using unittest, replacing pytest

--- test/test_repository.py
+++ test/test_repository.py
@@ -1,25 +1,22 @@
 import datetime
 import os
+import unittest
 
 import boto3
-import pytest
 from moto import mock_dynamodb2
 
 from app.repository import convert_date, find_last_birthdays, find_last_marriages
 
 
-@pytest.fixture(scope='function')
-def aws_credentials():
-    """Mocked AWS Credentials for moto."""
-    os.environ['AWS_ACCESS_KEY_ID'] = 'testing'
-    os.environ['AWS_SECRET_ACCESS_KEY'] = 'testing'
-    os.environ['AWS_SECURITY_TOKEN'] = 'testing'
-    os.environ['AWS_SESSION_TOKEN'] = 'testing'
+class TestRepository(unittest.TestCase):
+    def setUp(self):
+        """Mocked AWS Credentials for moto."""
+        os.environ['AWS_ACCESS_KEY_ID'] = 'testing'
+        os.environ['AWS_SECRET_ACCESS_KEY'] = 'testing'
+        os.environ['AWS_SECURITY_TOKEN'] = 'testing'
+        os.environ['AWS_SESSION_TOKEN'] = 'testing'
 
-
-@pytest.fixture(scope='function')
-def dynamodb():
-    with mock_dynamodb2():
+    def build_dynamodb(self):
         dynamodb = boto3.resource("dynamodb", region_name="us-east-1")
         dynamodb.create_table(
             TableName='member',
@@ -36,49 +33,54 @@ def dynamodb():
                 },
             ]
         )
-        yield dynamodb
+        return dynamodb
+
+    def test_convert_date(self):
+        formatted = convert_date(datetime.datetime.strptime("2020-05-10", "%Y-%m-%d"))
+        self.assertEqual("05-10", formatted)
+
+    @mock_dynamodb2
+    def test_find_last_birthdays_empty(self):
+        self.build_dynamodb()
+        now = datetime.date.today()
+        members = find_last_birthdays(now - datetime.timedelta(days=7), now)
+        self.assertEqual(0, len(members))
+
+    @mock_dynamodb2
+    def test_find_last_birthdays_with_values(self):
+        table = self.build_dynamodb().Table('member')
+        table.put_item(Item={
+            "id": 'xx',
+            "birthDateShort": convert_date(datetime.date.today()),
+            'firstName': 'Name',
+            'lastName': 'Last name'
+        })
+
+        now = datetime.date.today()
+        members = find_last_birthdays(now - datetime.timedelta(days=7), now)
+        self.assertEqual(1, len(members))
+
+    @mock_dynamodb2
+    def test_find_last_marriages_empty(self):
+        self.build_dynamodb()
+        now = datetime.date.today()
+        members = find_last_marriages(now - datetime.timedelta(days=7), now)
+        self.assertEqual(0, len(members))
+
+    @mock_dynamodb2
+    def test_find_last_birthdays_with_values(self):
+        table = self.build_dynamodb().Table('member')
+        table.put_item(Item={
+            "id": 'xx',
+            "marriageDateShort": convert_date(datetime.date.today()),
+            'firstName': 'Name',
+            'lastName': 'Last name'
+        })
+
+        now = datetime.date.today()
+        members = find_last_marriages(now - datetime.timedelta(days=7), now)
+        self.assertEqual(1, len(members))
 
 
-def test_convert_date():
-    formatted = convert_date(datetime.datetime.strptime("2020-05-10", "%Y-%m-%d"))
-    assert "05-10" == formatted
-
-
-def test_find_last_birthdays_empty(dynamodb):
-    now = datetime.date.today()
-    members = find_last_birthdays(now - datetime.timedelta(days=7), now)
-    assert 0 == len(members)
-
-
-def test_find_last_birthdays_with_values(dynamodb):
-    table = dynamodb.Table('member')
-    table.put_item(Item={
-        "id": 'xx',
-        "birthDateShort": convert_date(datetime.date.today()),
-        'firstName': 'Name',
-        'lastName': 'Last name'
-    })
-
-    now = datetime.date.today()
-    members = find_last_birthdays(now - datetime.timedelta(days=7), now)
-    assert 1 == len(members)
-
-
-def test_find_last_marriages_empty(dynamodb):
-    now = datetime.date.today()
-    members = find_last_marriages(now - datetime.timedelta(days=7), now)
-    assert 0 == len(members)
-
-
-def test_find_last_birthdays_with_values(dynamodb):
-    table = dynamodb.Table('member')
-    table.put_item(Item={
-        "id": 'xx',
-        "marriageDateShort": convert_date(datetime.date.today()),
-        'firstName': 'Name',
-        'lastName': 'Last name'
-    })
-
-    now = datetime.date.today()
-    members = find_last_marriages(now - datetime.timedelta(days=7), now)
-    assert 1 == len(members)
+if __name__ == '__main__':
+    unittest.main()
